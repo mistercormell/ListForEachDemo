9f07a1992deea95779e19eb69c50e7128fddbe92
David Cormell <d.cormell@etoncollege.org.uk>
2024-02-06 10:19:10 +0000

    Initial Commit

---
+++ ListForEachDemo/ListForEachDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListForEachDemoApp.swift
+//  ListForEachDemo
+//
+//  Created by Cormell, David - DPC on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ListForEachDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
